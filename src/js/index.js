282be49fae3b2d14ce0f9ee2f4df652d1c7923d7
Jennifer T. Nunes <jennifer_teles@hotmail.com>
2022-10-15 20:17:05 -0400

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,37 @@
+const abas = document.querySelectorAll(".aba");
+
+abas.forEach(aba => {
+    aba.addEventListener("click", function () {
+
+        if (aba.classList.contains("selecionado")) {
+            return;
+        }
+
+        selecionarAba(aba)
+        mostrarInformacoesdaAba(aba)
+
+  
+    });
+});
+
+function selecionarAba(aba) {
+
+    const abaSelecionada = document.querySelector(".aba.selecionado");
+    abaSelecionada.classList.remove("selecionado");
+
+    aba.classList.add("selecionado");
+
+}
+
+function mostrarInformacoesdaAba(aba){
+
+    const informacaoSelecionada = document.querySelector(".informacao.selecionado");
+    informacaoSelecionada.classList.remove("selecionado");
+
+    const idDoElementoDeInformacoesDaAba = `informacao-${aba.id}`
+
+    const informacaoASerMostrada = document.getElementById(idDoElementoDeInformacoesDaAba)
+    informacaoASerMostrada.classList.add("selecionado")
+
+
+}
